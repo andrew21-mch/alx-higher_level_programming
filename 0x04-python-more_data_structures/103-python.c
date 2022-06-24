62513d9e96ccf5532475fd08140d1b0cae68bdc7
andrew21-mch <nfonandrew73@gmail.com>
2022-06-24 22:29:31 +0100

    python exceptions

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,8 @@
+// Create two C functions that print some basic info about Python lists and Python bytes objects
+void print_python_list(PyObject *p){
+
+}
+
+void print_python_bytes(PyObject *p){
+    
+}
