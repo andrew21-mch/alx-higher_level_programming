fe09e308d344606f624517a7b5fb75fba0780a6e
andrew21-mch <nfonandrew73@gmail.com>
2022-12-11 08:41:33 +0100

    web scrapting advanced

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,23 +1,22 @@
 #!/usr/bin/node
-// Write a script that gets the contents of a webpage and stores it in a file.
+// Prints all characters in a specific Star Wars movie from the api
 
 const request = require('request');
-const fs = require('fs');
+const movie = process.argv[2];
+const url = 'https://swapi.co/api/films/' + movie;
 
-const requestURL = process.argv[2];
-const filePath = process.argv[3];
-
-request(requestURL, function (err, response, body) {
-    if (err) {
-        return console.log(err);
-    }
-    
-    fs.writeFile
-    (filePath, body, 'utf8', function (err) {
-        if (err) {
-            return console.log(err);
-        }
-    }
-    );
+request(url, function (err, res, body) {
+  if (err) {
+    console.log(err);
+  }
+  let film = JSON.parse(body);
+  for (let character of film.characters) {
+    request(character, function (err, res, body) {
+      if (err) {
+        console.log(err);
+      }
+      let chr = JSON.parse(body);
+      console.log(chr.name);
+    });
+  }
 });
-
